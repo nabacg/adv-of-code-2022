89501ec43be91447176857a364be63fa37985943
gcaban <greg.caban@liveramp.com>
2022-12-23 12:06:16 +0100

    Day 3 part 1 solution

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
     error::Error,
     fs::{self},
     io::{BufRead, BufReader},
-    process,
+    process, collections::HashSet,
 };
 
 // DAY 1
@@ -208,6 +208,59 @@ fn day2_result(lines: Vec<String>) -> Result<(), Box<dyn Error>> {
 }
 // DAY 2
 
+// DAY 3 
+
+pub struct Rucksack {
+    compartment_a: Vec<char>,
+    compartment_b: Vec<char>,
+}
+
+impl Rucksack {
+pub fn from_input(l: &String) -> Result<Rucksack, &str> {
+        if l.chars().count() % 2 != 0 {
+            return Err("Invalid input, only even character counts are supported")
+        }
+        let (a, b) =    l.split_at(l.chars().count()/2);
+        Ok(Rucksack { compartment_a: a.chars().collect(), compartment_b: b.chars().collect() })
+    }
+
+    pub fn overlaps(&self) -> Vec<&char> {
+        let comp_a_set= HashSet::<_>::from_iter(self.compartment_a.iter());
+        let dedup = HashSet::<_>::from_iter(self.compartment_b.iter().filter(|b| comp_a_set.contains(b)));
+        dedup.iter().map(|c| *c).collect()
+    }
+
+    pub fn item_priority(i: &char) -> u32 {
+        // println!("i: {}",i);
+        let ascii = *i as u32;
+        if i.is_uppercase() {
+            ascii - 38 // - 65 + 26
+        } else {
+            ascii - 96
+        }
+  
+    }
+}
+
+fn day3_result(lines: Vec<String>) -> Result<(), Box<dyn Error>> {
+    let rucksacks: Result<Vec<Rucksack>, &str> = lines.iter().map(Rucksack::from_input).collect();
+    match rucksacks {
+        Ok(rucksacks) => { 
+            let misplaced_items: u32 = rucksacks.iter()
+                .flat_map(|r| r.overlaps())
+                .map(Rucksack::item_priority)
+                .sum();
+            println!("result is {}", misplaced_items);
+        }
+        Err(e) => {
+            eprintln!("error processing input: {}", e);
+        }
+    }
+    Ok(())
+}
+
+// DAY 3 END
+
 fn read_lines(input_path: &str) -> Result<Vec<String>, std::io::Error> {
     let input_file = fs::File::open(input_path)?;
     let lines = BufReader::new(input_file).lines();
@@ -215,9 +268,12 @@ fn read_lines(input_path: &str) -> Result<Vec<String>, std::io::Error> {
     lines.collect()
 }
 
+
+
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    // let input_path =    "test_input.txt";
+    // let input_path =    "inputs/day3/test_input.txt";
+    // let aoc_day = "day3".to_string();
     if args.len() < 3 {
         eprintln!("2 cmd argument required:\n - day of Advent of Code puzzle, in day1, day2,etc format\n - path to the input text file");
         process::exit(1);
@@ -230,6 +286,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     match aoc_day.as_str() {
         "day1" => day1_result(lines),
         "day2" => day2_result(lines),
+        "day3" => day3_result(lines),
         _d => {
             eprintln!("Not implemented Advent Of Code Day selected: {}, currently only [day1,day2] are supported ", aoc_day);
             process::exit(1)
