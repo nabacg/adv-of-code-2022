25b43761249b25d2ffcbaaf05909cb594933f159
Grzegorz Caban <nabacg@gmail.com>
2023-01-07 20:05:57 +0000

    Day10, part1 solution 1 ( changes missed in previous commit)

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
+mod day10;
 
 extern crate pest;
 #[macro_use]
@@ -33,6 +34,7 @@ pub fn print_result(aoc_day: &String, input_path: &String) -> Result<(), Box<dyn
         "day7" => day7::result(fs::read_to_string(input_path)?),
         "day8" => day8::result(fs::read_to_string(input_path)?),
         "day9" => day9::result(read_lines(input_path)?),
+        "day10" => day10::result(read_lines(input_path)?),
         _d => 
             Err(format!("Not implemented Advent Of Code Day selected: {}, currently only [day1,day2] are supported ", aoc_day).into())
             
