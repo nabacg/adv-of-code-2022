079e5fe2673904424a0f2453702f6539bbfcb773
Grzegorz Caban <nabacg@gmail.com>
2023-01-07 19:23:22 +0000

    Solutions to Day8 (part1 and part2) and Day9 (part 1 and 2)

---
+++ src/day8.rs
@@ -0,0 +1,204 @@
+use core::fmt;
+use std::{num::ParseIntError, ops::Index, error::Error};
+
+// thanks https://stackoverflow.com/a/70511530
+#[derive(Debug)]
+pub struct Vec2d<T> {
+    vec: Vec<T>,
+    row: usize,
+    col: usize,
+}
+
+impl<T> Vec2d<T> {
+    pub fn new(vec: Vec<T>, row: usize, col: usize) -> Self {
+        assert!(vec.len() == row * col);
+        Self { vec, row, col }
+    }
+
+    pub fn row(&self, row: usize) -> &[T] {
+        let i = self.col * row;
+        &self.vec[i..(i + self.col)]
+    }
+
+    pub fn index(&self, row: usize, col: usize) -> &T {
+        let i = self.col * row;
+        &self.vec[i + col]
+    }
+
+    pub fn index_mut(&mut self, row: usize, col: usize) -> &mut T {
+        let i = self.col * row;
+        &mut self.vec[i + col]
+    }
+}
+
+impl<T: std::fmt::Debug> std::fmt::Display for Vec2d<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut str = String::new();
+        for i in 0..self.row {
+            if i != 0 {
+                str.push_str(", ");
+            }
+            str.push_str(&format!("{:?}", &self.row(i)));
+        }
+        write!(f, "[{}]", str)
+    }
+}
+
+struct TreeGrid {
+    tree_heights: Vec2d<u32>
+}
+
+impl TreeGrid {
+    fn new(ls: String) -> Result<TreeGrid, String> {
+        const RADIX: u32 = 10;
+        let cols = ls.find('\n').ok_or("Invalid input - single line tree grid is not a grid")?;
+        let rows = ls.chars().filter(|&c| c == '\n').count()+1;
+        let data: Result<Vec<u32>, String> = 
+
+        ls.chars().filter(|&c| c != '\n').map(|c| c.to_digit(RADIX).ok_or(format!("failed to parse char: {}", c))).collect::<Result<Vec<u32>, String>>();
+
+        Ok(TreeGrid { tree_heights: Vec2d::new(data?, rows, cols) })
+    }
+
+    fn inner_trees(&self) -> Vec<TreeCoord> {
+        (1..self.tree_heights.col-1).into_iter()
+        .flat_map(|col| (1..self.tree_heights.row-1).map(|r| TreeCoord{ x: r, y: col}).collect::<Vec<TreeCoord>>())
+        .collect()
+    }
+
+    fn height(&self, c: &TreeCoord) -> &u32 {
+        self.tree_heights.index(c.x, c.y)
+    }
+
+    fn edge_tree_count(&self) -> usize {
+        2 * self.tree_heights.col + 2 * (self.tree_heights.row - 2) // 2 full col lengths plus 2 * row lengths - 2( to prevent double counting corners ) )
+    }
+
+}
+
+#[derive(Debug)]
+struct TreeCoord {
+    x: usize,
+    y: usize,
+}
+
+impl fmt::Display for TreeCoord {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[{}, {}]", self.x, self.y)
+    }
+}
+
+impl TreeCoord {
+    fn is_visible(&self, tg: &TreeGrid) -> bool {
+        let this_height = tg.height(self);
+        let right = VizPath::right(self,   tg.tree_heights.col);
+        let left = VizPath::left(self);
+        let up = VizPath::up(self);
+        let down = VizPath::down(self, tg.tree_heights.row);
+
+
+        !vec![left, up, right, down].iter().all(|p| {
+            let covered =  p.path.iter().map(|c|tg.height(c)).any(|h| h >= this_height);
+                // println!("Tree {} is {} visible from {}", self, if covered { "Not"} else {""}, p.dir);
+            covered
+        })
+    }
+
+    fn scenic_score(&self, tg: &TreeGrid) -> usize {
+        let this_height = tg.height(self);
+        let right = VizPath::right(self,   tg.tree_heights.col);
+        let left = VizPath::left(self);
+        let up = VizPath::up(self);
+        let down = VizPath::down(self, tg.tree_heights.row);
+
+        vec![left, up, right, down]
+        .iter()
+        .map(|p| {
+            let scenic_path = p.path.iter().take_while(|&c| { 
+                let is_lower = tg.height(c) < this_height;
+                // println!("\t{}, with height: {} is lower? {} than {}", c, tg.height(c), is_lower, this_height);
+                is_lower
+            }).collect::<Vec<&TreeCoord>>();
+            let unobscuring_tree_count = scenic_path.iter().count();
+            
+            let scenic_count = if unobscuring_tree_count == p.path.len() {
+                // unobscured view till edge 
+                unobscuring_tree_count
+            } else {
+                // we need to add the 1 tree that obscured the view
+                unobscuring_tree_count + 1
+            };
+            println!("Tree: {} has scenic count of: {} from {}", self, scenic_count, p.dir);
+            // if scenic_count == 0 {
+            //     1
+            // } else {
+            //     scenic_count
+            // }
+            scenic_count
+        })
+        .fold(1, std::ops::Mul::mul )
+    }
+}
+
+struct VizPath {
+    path: Vec<TreeCoord>,
+    dir: String,
+}
+
+impl VizPath {
+    fn down(c: &TreeCoord, grid_rows: usize) -> VizPath {
+        VizPath {
+            dir: "DOWN".to_string(),
+            path:
+            (c.x+1..grid_rows).map(|r| TreeCoord {x: r, y: c.y}).collect()
+        }   
+    } 
+
+    fn up(c: &TreeCoord) -> VizPath {
+        VizPath {
+            dir: "UP".to_string(),
+            path:
+            (0..c.x).rev() // it's easier if path Coords always go from TreeCord to the edge
+            .map(|r| TreeCoord {x: r, y: c.y}).collect()
+        }   
+    } 
+
+    fn right(c: &TreeCoord, grid_cols: usize) -> VizPath {
+        VizPath {
+            dir: "RIGHT".to_string(),
+            path:
+            (c.y+1..grid_cols).map(|col| TreeCoord {x: c.x, y: col}).collect()
+        }   
+    } 
+    fn left(c: &TreeCoord) -> VizPath {
+        VizPath {
+            dir: "LEFT".to_string(),
+            path:
+            (0..c.y)
+            .rev() // it's easier if path Coords always go from TreeCord to the edge
+            .map(|col| TreeCoord {x: c.x, y: col}).collect()
+        }   
+    } 
+}
+
+pub fn result(input: String) -> Result<(), Box<dyn Error>> {
+    let tg = TreeGrid::new(input)?;
+    let inner_ts = tg.inner_trees();
+    let visible_inner_trees:Vec<&TreeCoord> = inner_ts.iter().filter(|&tc| tc.is_visible(&tg)).collect();
+    // println!("{:?}", visible_inner_trees);
+    let inner_count =    visible_inner_trees.len();
+    println!("part1 - result: {}", inner_count + tg.edge_tree_count());
+
+    let most_scenic_tree = inner_ts.iter()
+        .max_by(|a,b| a.scenic_score(&tg).cmp(&b.scenic_score(&tg)))
+        .ok_or("cannot find max_scenic_score, empty inner trees ?")?;
+
+    println!("part2 - result: {}, tree: {}", most_scenic_tree.scenic_score(&tg), most_scenic_tree);
+    Ok(())
+
+
+
+        // // let test = TreeCoord{ x:1, y: 2};
+        // println!("part2 - result: {}, tree: {}", test.scenic_score(&tg), test);
+        // Ok(())
+}
---
+++ src/day9.rs
@@ -0,0 +1,276 @@
+use core::fmt;
+use std::{error::Error, collections::{HashSet, HashMap}};
+
+#[derive(Debug)]
+enum Dir {
+    NoOp,
+    Up,
+    Down,
+    Left,
+    Right,
+    LeftUp,
+    RightUp,
+    LeftDown,
+    RightDown,
+}
+
+impl Dir {
+    fn new(d: &str) -> Result<Dir, String> {
+        match d {
+            "U" => Ok(Dir::Up),
+            "D" => Ok(Dir::Down),
+            "L" => Ok(Dir::Left),
+            "R" => Ok(Dir::Right),
+            _ => Err(format!("Invalid input, expected one of U | D | L | R, got: {} ", d))
+        }
+    }
+}
+
+struct Move {
+    dir: Dir,
+    steps: u16,
+}
+
+impl Move {
+    fn new(line: String) -> Result<Move, String> {
+        let parts = line.split_whitespace().collect::<Vec<&str>>();
+        if parts.len() != 2 {
+            Err(format!("Invalid input, expected whitespace separated 2 part line, Dir and Num_of_steps, got {}", line))
+        } else {
+
+            let dir = Dir::new(parts[0])?;
+            let steps = parts[1].parse::<u16>().map_err(|e| format!("{}", e))?;
+            Ok(Move { dir: dir, steps: steps })
+        }
+
+    }
+}
+
+#[derive(Copy, Clone, Hash, PartialEq, Eq)]
+struct Knot {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Display for Knot {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
+    }
+}
+
+impl Knot {
+    fn new(x: i32, y: i32) -> Knot {
+        Knot {
+            x: x, 
+            y: y,
+        }
+    }
+
+    fn move_to(&self, d: &Dir) -> Knot {
+        match d {
+            Dir::Up    => Knot::new(self.x, self.y+1),
+            Dir::Down  => Knot::new(self.x, self.y-1),
+            Dir::Left  => Knot::new(self.x-1, self.y),
+            Dir::Right => Knot::new(self.x+1, self.y),
+            Dir::NoOp =>  *self,
+            Dir::LeftUp => Knot::new(self.x-1, self.y+1),
+            Dir::RightUp => Knot::new(self.x+1, self.y+1),
+            Dir::LeftDown => Knot::new(self.x-1, self.y-1),
+            Dir::RightDown => Knot::new(self.x+1, self.y-1),
+        }
+    }
+
+    fn distance(&self, r: &Knot) -> f64 {
+        let x_delta = (r.x - self.x) as f64; 
+        let y_delta = (r.y - self.y) as f64;
+        (x_delta.powi(2) + y_delta.powi(2)).sqrt()
+    }
+
+    fn is_adjacent(&self, other: &Knot) -> bool {
+        self.x.abs_diff(other.x) <= 1 &&
+            self.y.abs_diff(other.y) <= 1
+    }
+
+    fn eq_row(&self, other: &Knot) -> bool {
+        self.x == other.x
+    }
+
+
+    fn eq_col(&self, other: &Knot) -> bool {
+        self.y == other.y 
+    }
+
+    fn is_above(&self, other: &Knot) -> bool {
+        other.y > self.y 
+    }
+
+    fn is_below(&self, other: &Knot) -> bool {
+        other.y < self.y
+    }
+
+    fn is_left(&self, other: &Knot) -> bool {
+        other.x < self.x 
+    }
+
+    fn is_right(&self, other: &Knot) -> bool {
+        other.x > self.x 
+    }
+
+
+    fn dir_towards(&self, other: &Knot) -> Dir {
+        match (self, other) {
+            (a, b) if a.eq_col(b)   && a.eq_row(b) => Dir::NoOp,
+            (a, b) if a.is_left(b)  && a.is_above(b)        =>  Dir::LeftUp,
+            (a, b) if a.is_right(b)  && a.is_above(b)        =>  Dir::RightUp,
+            (a, b) if a.is_left(b)  && a.is_below(b)        =>  Dir::LeftDown,
+            (a, b) if a.is_right(b)  && a.is_below(b)        =>  Dir::RightDown,
+            (a, b) if a.is_above(b)        =>  Dir::Up,
+            (a, b) if a.is_below(b)        =>  Dir::Down,
+            (a, b) if a.is_left(b)          =>  Dir::Left,
+            (a, b) if a.is_right(b)         =>  Dir::Right,
+
+             _ => unreachable!()
+        }
+    }
+}
+
+struct Rope {
+    // head: Knot,
+    // tail: Knot,
+    knots: Vec<Knot>,
+    end_knot_history: Vec<Knot>,
+    end_knot_index: usize,
+}
+
+impl Rope {
+    fn new(knots_count: usize) -> Rope {
+        Rope {           
+            knots: (0..knots_count).map(|_| Knot::new(0,0)).collect(),
+            end_knot_index: knots_count-1,
+            end_knot_history: vec![],
+        }
+    }
+
+    fn head(&self) -> &Knot {
+        &self.knots[0]
+    }
+
+    fn tail(&self) -> &Knot {
+        &self.knots[self.end_knot_index]
+    }
+
+    fn apply_move(&mut self, m: &Move) {
+        for i in 0..m.steps {
+            self.apply_dir(&m.dir);
+        }
+    }
+
+    fn apply_dir(&mut self, dir: &Dir) {
+        println!("{}", self);
+        let new_head = self.knots[0].move_to(dir);
+        
+        let mut knots = vec![new_head];
+        let new_knots = self.knots[1..].iter()
+            .fold(&mut knots, |acc,  k| {
+                let head = acc.last()
+                    .expect("Should never be empty, we start from [1..]");
+                let tail = k;
+                println!("Head: {}, Tail: {}", head, tail);
+                if !head.is_adjacent(tail) {
+                    let tail_move = tail.dir_towards(&head);
+                    println!("Tail Move: {:?}", tail_move);
+                    let new_tail = tail.move_to(&tail_move);
+                    acc.push(new_tail);
+                    acc
+                } else {
+                    acc.push(*k);
+                    acc
+                }
+        });
+        self.end_knot_history.push(self.tail().clone());
+        self.knots = knots;
+    }
+
+    fn move_knot(head: &Knot, tail: &Knot, dir: &Dir) -> (Knot, Knot) {
+        let new_head = head.move_to(dir);
+        if !new_head.is_adjacent(tail) {
+            let tail_move = tail.dir_towards(&new_head);
+            println!("Tail Move: {:?}", tail_move);
+            let new_tail = tail.move_to(&tail_move);
+            (new_head, new_tail)
+        } else {
+            (new_head, tail.clone())
+        }
+    }
+
+    fn print_tail_history(&self)  {
+        let head = self.head();
+        let tail = self.tail();
+        let max_rows = head.x.max(tail.x).max(5);
+        let max_cols = head.y.max(tail.y).max(6);
+
+        (0..max_rows).rev().for_each(|y| {
+            let row_str:Vec<&str> = (0..max_cols).map(|x| {
+                if x == 0 && y == 0 {
+                    "s"
+                }
+                else if self.end_knot_history.contains(&Knot::new(x, y)) {
+                    "#"
+                }
+                else {
+                    "."
+                }
+            }).collect();
+            println!("{}", row_str.join(""));
+        });
+    }
+}
+
+impl fmt::Display for Rope {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let head = self.head();
+        let tail = self.tail();
+        writeln!(f, "Head: {}, Tail: {}", head, tail);
+        let max_cols =  self.knots.iter().map(|k| k.x).max().unwrap(); 
+        let max_rows =  self.knots.iter().map(|k| k.y).max().unwrap();
+
+        let max_cols = max_cols.max(6);
+        let max_rows = max_rows.max(5);
+
+        let knots_lookup: HashMap<(i32, i32), String> = HashMap::from_iter(self.knots.iter().enumerate().map(|(i, k)|{
+            if i == 0 {
+                ((k.x, k.y), "H".to_string())
+            } else  {
+                ((k.x, k.y), i.to_string())
+            }
+        }));
+
+        (0..max_rows).rev().for_each(|y| {
+            let row_str:Vec<&str> = (0..max_cols).map(|x| {
+                if knots_lookup.contains_key(&(x, y)) {
+                    knots_lookup.get(&(x,y)).unwrap()
+                }
+                else {
+                    "."
+                }
+            }).collect();
+            writeln!(f, "{}", row_str.join(""));
+        });
+        writeln!(f, "")
+        
+    }
+}
+
+
+pub fn result(lines: Vec<String>) -> Result<(), Box<dyn Error>> {
+
+    let moves: Result<Vec<Move>, String> = lines.into_iter().map(Move::new).collect();
+    let mut r = Rope::new(10);
+
+    moves?.iter().for_each(|m| r.apply_move(m));
+    r.end_knot_history.push(r.tail().clone()); // don't forget to add last tail position to history
+    r.print_tail_history();
+    let unique_tail_coords = HashSet::<_>::from_iter(r.end_knot_history);
+    println!("Part1 Result: {}", unique_tail_coords.len());
+    Ok(())
+}
