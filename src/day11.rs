7f4d5595ab48b31e5a67ebae3e2d9645e8187100
Grzegorz Caban <nabacg@gmail.com>
2023-01-09 22:53:17 +0000

    Day11 Part2 solution with some clever numerical tricks

--- src/day11.rs
+++ src/day11.rs
@@ -1,15 +1,17 @@
+use core::fmt;
 use std::{error::Error, num::ParseIntError, collections::HashMap};
 use pest::{Parser, iterators::Pair};
+use itertools::Itertools;
 
 #[derive(Debug, Clone)]
 struct MonkeyTest {
-    param: i32,
+    param: u64,
     truthy_target: usize,
     falsy_target: usize,
 }
 impl MonkeyTest {
-    fn apply(&self, new_worry_level: i32) -> usize {
-        if new_worry_level % self.param == 0{
+    fn apply(&self, new_worry_level: u64) -> usize {
+        if new_worry_level % self.param == 0 {
             self.truthy_target
         } else {
             self.falsy_target
@@ -20,7 +22,7 @@ impl MonkeyTest {
 #[derive(Debug, Clone)]
 enum MonkeyOpArg {
     Old,
-    IntArg(i32),
+    IntArg(u64),
 }
 
 #[derive(Debug, Clone)]
@@ -33,17 +35,17 @@ impl MonkeyOp {
         MonkeyOp { arg, operator: op }
     }
 
-    fn apply(&self, worry_level: i32) -> i32 {
+    fn apply(&self, worry_level: u64) -> u64 {
         match self.operator.as_str() {
             "+" => self.arg0(worry_level) + worry_level,
-            "*" => self.arg0(worry_level) * worry_level,
+            "*" =>  self.arg0(worry_level) * worry_level,
             _ => panic!("invalid Opertor found, only (+ | * ) are suuported, got {}", self.operator),
         }
     }
 
-    fn arg0(&self, arg1: i32) -> i32 {
+    fn arg0(&self, arg1: u64) -> u64 {
         match self.arg {
-            MonkeyOpArg::Old => arg1,
+            MonkeyOpArg::Old => arg1.clone(),
             MonkeyOpArg::IntArg(arg0) => arg0,
         }
     }
@@ -52,14 +54,20 @@ impl MonkeyOp {
 #[derive(Debug)]
 struct Monkey {
     id: usize,
-    items: Vec<i32>,
+    items: Vec<u64>,
     operation: MonkeyOp,
     test: MonkeyTest,
     inspected_items: usize,
 }
 
+impl fmt::Display for Monkey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Items: {:?}, Inspected: {}", self.items, self.inspected_items)
+    }
+}
+
 impl Monkey {
-    fn new(id: usize, items: Vec<i32>, op: MonkeyOp, test: MonkeyTest) -> Monkey {
+    fn new(id: usize, items: Vec<u64>, op: MonkeyOp, test: MonkeyTest) -> Monkey {
         Monkey{ id: id,
              items: items, 
              operation: op, 
@@ -67,48 +75,64 @@ impl Monkey {
              inspected_items:0 }
     }
 
-    fn next_round(&self, inspected: usize) -> Monkey {
-        Monkey{ id: self.id,
-            items: vec![], 
-            operation: self.operation.clone(), 
-            test: self.test.clone(), 
-            inspected_items:self.inspected_items + inspected }
-    }
-
-    fn round(&self) -> (Monkey, Vec<(usize, i32)>) {
-        let thrown_items: Vec<(usize, i32)> =        self.items.iter().map(|&i| {
+    fn round(&self, div: &u64) -> (usize, Vec<(usize, u64)>) {
+        let thrown_items: Vec<(usize, u64)> =        self.items.iter().map(|&i| {
             let new_worry_level = self.operation.apply(i);
-            let new_worry_level = new_worry_level / 3; // This operation rounds towards zero,  so I think we're fine?
+            //let new_worry_level = new_worry_level / 3; // This operation rounds towards zero,  so I think we're fine?
+
+            // use Chinese Reminder Theorem https://brilliant.org/wiki/chinese-remainder-theorem/ 
+            // since all divisors are prime, we can find their GCD by multiplying them and then use this GCD to modulo the worry level
+            //  without affecting divisability test
+            // thanks Reddit https://www.reddit.com/r/adventofcode/comments/zifqmh/comment/j26b81u/?utm_source=share&utm_medium=web2x&context=3
+            let new_worry_level = new_worry_level % div;
             let target_monkey = self.test.apply(new_worry_level);
             (target_monkey, new_worry_level)
         }).collect();
 
-        (self.next_round(thrown_items.len()),   thrown_items)
+        (thrown_items.len(),   thrown_items)
     }
 
-    fn catch_item(&mut self, item: i32) {
+    fn catch_item(&mut self, item: u64) {
         self.items.push(item);
     }
 }
 
 struct MonkeyGame {
     monkeys: Vec<Monkey>,
+    gcd: u64,
 
 }
 
 impl MonkeyGame {
-    fn new(monkeys: Vec<Monkey>) -> MonkeyGame {
-        MonkeyGame { monkeys, }
+
+    fn print(&self) {
+        let ms = self.monkeys.iter().map(|m| format!("{}", m)).collect::<Vec<String>>();
+        println!("Monkeys:\n{}\n", ms.join(""));
     }
+
+    fn new(monkeys: Vec<Monkey>) -> MonkeyGame {
+        let gcd = monkeys.iter().map(|m| m.test.param).product();
+        MonkeyGame { monkeys, gcd}
+    }
+
     fn round(&mut self) {
-        for m in &self.monkeys {
-            let (new_m, thrown_items) = m.round();
-            println!("new_m: {:?}, thrown_items: {:?}", new_m, thrown_items);
-            // for (target_monkey_id, item) in thrown_items {
-            //     let target_monkey = self.monkeys.get(&target_monkey_id).unwrap();
-            //     let mut new_target_monkey = target_monkey.clone(); 
-            //     (&new_target_monkey).catch_item(item);
-            // }
+        for i in 0..self.monkeys.len() {
+            // self.print();
+            let (inspected_items, thrown_items) = self.monkeys[i].round(&self.gcd);
+            //  println!("inspected_items: {}, thrown_items: {:?}", inspected_items, thrown_items);
+            for (target_monkey_id, item) in thrown_items {
+                let target_monkey =
+                                 self.monkeys
+                                    .get_mut(target_monkey_id)
+                                    .expect("Item thrown to unknown Monkey.");
+                target_monkey.catch_item(item);
+            }
+            let update_m = self.monkeys.get_mut(i).unwrap();
+            update_m.inspected_items += inspected_items;
+            update_m.items.clear();
+
+
+           
         }
     }
 }
@@ -120,7 +144,13 @@ struct MonkeySpecParser;
 pub(crate) fn result(input: String) -> Result<(), Box<dyn Error>> {
     let monkeys = parse_input(&input)?;
     let mut mg = MonkeyGame::new(monkeys);
-    mg.round();
+    for i in 0..10000 {
+        println!("Round: {}", i);
+        mg.round();
+    }
+    mg.print();
+    let monkey_business:Vec<usize> = mg.monkeys.iter().map(|m| m.inspected_items).sorted().rev().take(2).collect();
+    println!("Part 1 Result: {:?}", monkey_business.iter().product::<usize>());
     Ok(())
 }
 
@@ -142,8 +172,8 @@ fn parse_monkey(p: Pair<Rule>) -> Result<Monkey, String> {
     let starting_items_spec = pairs.next().ok_or("Invalid input expected startingItemsSpec")?;
     let starting_items = match starting_items_spec.as_rule() {
         Rule::startingItemsSpec => {
-                let starting_items: Result<Vec<i32>, String> =   starting_items_spec.into_inner()
-                    .map(|i| i.as_str().parse::<i32>().map_err(|e| format!("{}", e))).collect();
+                let starting_items: Result<Vec<u64>, String> =   starting_items_spec.into_inner()
+                    .map(|i| i.as_str().parse::<u64>().map_err(|e| format!("{}", e))).collect();
                 let starting_items  = starting_items?;
                 Ok(starting_items)                
 
@@ -163,7 +193,7 @@ fn parse_monkey(p: Pair<Rule>) -> Result<Monkey, String> {
             let coef:Result<MonkeyOpArg, String> =   {
                 let param_p = operation_params.next().ok_or("Invalid input, operation_spec needs operationCoef ")?;
                 match param_p.as_rule() {
-                   Rule::operationIntArg =>  Ok(MonkeyOpArg::IntArg(param_p.as_str().parse::<i32>().map_err(|e| format!("{}", e))?)),
+                   Rule::operationIntArg =>  Ok(MonkeyOpArg::IntArg(param_p.as_str().parse::<u64>().map_err(|e| format!("{}", e))?)),
                    Rule::operationOldSelfArg => Ok(MonkeyOpArg::Old),
                    _ => Err("Invalid input, operationSpec operationArg".into()),
                 }
@@ -178,8 +208,8 @@ fn parse_monkey(p: Pair<Rule>) -> Result<Monkey, String> {
     let test_spec = pairs.next().ok_or("Invalid input - expected testSpec")?;
     let monkey_test:Result<MonkeyTest, String> = match test_spec.as_rule() {
              Rule::testSpec => {
-                let digits: Result<Vec<i32>, String> =        test_spec.into_inner()
-                    .map(|d| d.as_str().parse::<i32>().map_err(|e| format!("{}", e)))
+                let digits: Result<Vec<u64>, String> =        test_spec.into_inner()
+                    .map(|d| d.as_str().parse::<u64>().map_err(|e| format!("{}", e)))
                     .collect();
                 let digits = digits?;
                 Ok(MonkeyTest{
